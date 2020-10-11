dca0b219735b3e733f2c4223540fe321c7882df2
Alan Echeveste <alan.echeveste2963@alumnos.udg.mx>
2020-10-11 17:14:30 -0500

    Creacion de objetos en main

--- main.cpp
+++ main.cpp
@@ -1,10 +1,36 @@
 #include <iostream>
-#include "Computadora.h"
+#include "Laboratorio.h"
 
 using namespace std;
 
 int main()
 {
-    Computadora c1 = Computadora("guindows", "asrock", 8, 170);
+    Computadora c1 = Computadora("Windows", "HP", 8, 500);
+
+    Computadora c2; // =Computadora();
+    c2.setSistema("Windows");
+    c2.setMarca("asRock");
+    c2.setRam(16);
+    c2.setCapacidad(2150);
+
+    Computadora c3;
+    c3.setSistema("ios");
+    c3.setMarca("mac");
+    c3.setRam(16);
+    c3.setCapacidad(1000);
+
+
+    /*cout<<c2.getSistema()<<endl;
+    cout<<c2.getMarca()<<endl;
+    cout<<c2.getRam()<<endl;
+    cout<<c2.getCapacidad()<<endl;*/
+
+    Laboratorio lb;
+
+    lb.agregaComputadora(c1);
+    lb.agregaComputadora(c2);
+    lb.agregaComputadora(c3);
+
+    lb.mostrar();
     return 0;
 }
