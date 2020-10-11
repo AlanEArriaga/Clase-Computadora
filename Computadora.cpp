9d1f0d88118a72363f1188ba78ed1e6a2afc019d
Alan Echeveste <alan.echeveste2963@alumnos.udg.mx>
2020-10-11 16:32:35 -0500

    Implementacion de getters y setters en .cpp

--- Computadora.cpp
+++ Computadora.cpp
@@ -12,3 +12,30 @@ Computadora::Computadora(const string &sistema, const string &marca, int ram, in
     this->ram= ram;
     this->capacidad = capacidad;
 }
+
+void Computadora::setSistema(const string &v){
+    sistema = v;
+}
+
+string Computadora::getSistema(){
+    return sistema;
+}
+
+void Computadora::setMarca(const string &v){
+    marca = v;
+}
+string Computadora::getMarca(){
+    return marca;
+}
+void Computadora::setRam(int v){
+    ram = v;
+}
+int Computadora::getRam(){
+    return ram;
+}
+void Computadora::setCapacidad(int v){
+    capacidad = v;
+}
+int Computadora::getCapacidad(){
+    return capacidad;
+}
